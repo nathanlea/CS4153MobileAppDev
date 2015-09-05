a7ecf437ce70491248091fd46270ca96cc224bca
Nathan <nathan@Administrators-Mac-mini.local>
2015-09-05 10:56:13 -0500

    Remove .DS_Store and added WIA3

---
+++ WIA/WIA3_lea_nathan/WIA3_lea_nathan/MyView.swift
@@ -0,0 +1,17 @@
+//
+//  MyView.swift
+//  WIA3_lea_nathan
+//
+//  Created by Nathan on 9/5/15.
+//  Copyright © 2015 Okstate. All rights reserved.
+//
+
+import UIKit
+
+class MyView : UIView {
+    
+    
+    
+}
+
+
