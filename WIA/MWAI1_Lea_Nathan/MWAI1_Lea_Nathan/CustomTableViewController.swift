d5c8947f40dd4c338269b7758d28ecf29bbd73ba
Nathan <nathan.lea@okstate.edu>
2015-10-02 15:53:41 -0500

    MWIA working push, just need comments

--- WIA/MWAI1_Lea_Nathan/MWAI1_Lea_Nathan/CustomTableViewController.swift
+++ WIA/MWAI1_Lea_Nathan/MWAI1_Lea_Nathan/CustomTableViewController.swift
@@ -12,74 +12,74 @@ class CustomTableViewController: UITableViewController {
     
     typealias stateTuple = (name: String, nickname: String, imageFileName: String, visited: Bool)
     var stateArray: [[stateTuple]] =
-    [[( ( name: "Alabama", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false ) ),
-        ( (name: "Alaska", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Arizona", nickname: "WE have grand canyon", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Arkansas", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [[( ( name: "Alabama", nickname: "Lizard State", imageFileName: "alabama-physical-map", visited: false ) ),
+        ( (name: "Alaska", nickname: "The Last Frontier", imageFileName: "alaska-physical-map", visited: false) ),
+        ( (name: "Arizona", nickname: "Grand Canyon State", imageFileName: "arizona-physical-map", visited: false) ),
+        ( (name: "Arkansas", nickname: "The Natural State", imageFileName: "arkansas-physical-map", visited: false) )],
         
-    [( (name: "California", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Colorado", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Connecticut", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "California", nickname: "The Golden State", imageFileName: "california-physical-map", visited: false) ),
+        ( (name: "Colorado", nickname: "Centennial State", imageFileName: "colorado-physical-map", visited: false) ),
+        ( (name: "Connecticut", nickname: "Constitution State", imageFileName: "connecticut-physical-map", visited: false) )],
         
-    [( (name: "Delaware", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Delaware", nickname: "The First State", imageFileName: "delaware-physical-map", visited: false) )],
         
-    [( (name: "Florida", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Florida", nickname: "Sunshine State", imageFileName: "florida-physical-map", visited: false) )],
         
-    [( (name: "Georgia", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Georgia", nickname: "Peach State", imageFileName: "georgia-physical-map", visited: false) )],
         
-    [( (name: "Hawaii", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Hawaii", nickname: "Aloha State", imageFileName: "hawaii-physical-map", visited: false) )],
         
-    [( (name: "Idaho", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Illinois", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Indiana", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Iowa", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Idaho", nickname: "Gem State", imageFileName: "idaho-physical-map", visited: false) ),
+        ( (name: "Illinois", nickname: "Prairie State", imageFileName: "illinois-physical-map", visited: false) ),
+        ( (name: "Indiana", nickname: "Hoosier State", imageFileName: "indiana-physical-map", visited: false) ),
+        ( (name: "Iowa", nickname: "Hawkeye State", imageFileName: "iowa-physical-map", visited: false) )],
         
-    [( (name: "Kansas", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Kentucky", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Kansas", nickname: "Sunflower State", imageFileName: "kansas-physical-map", visited: false) ),
+        ( (name: "Kentucky", nickname: "Bluegress State", imageFileName: "kentucky-physical-map", visited: false) )],
     
-    [( (name: "Louisiana", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Louisiana", nickname: "Pelican State", imageFileName: "louisiana-physical-map", visited: false) )],
         
-    [( (name: "Maine", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Maryland", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Massachusetts", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Michigan", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Minnesota", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Mississippi", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Missouri", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Montana", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Maine", nickname: "Pine Tree State", imageFileName: "maine-physical-map", visited: false) ),
+        ( (name: "Maryland", nickname: "Old Line State", imageFileName: "maryland-physical-map", visited: false) ),
+        ( (name: "Massachusetts", nickname: "The Bay State", imageFileName: "massachusetts-physical-map", visited: false) ),
+        ( (name: "Michigan", nickname: "The Great Lakes State", imageFileName: "michigan-physical-map", visited: false) ),
+        ( (name: "Minnesota", nickname: "North Star State", imageFileName: "minnesota-physical-map", visited: false) ),
+        ( (name: "Mississippi", nickname: "Magnolia State", imageFileName: "mississippi-physical-map", visited: false) ),
+        ( (name: "Missouri", nickname: "Show-Me State", imageFileName: "missouri-physical-map", visited: false) ),
+        ( (name: "Montana", nickname: "Treasure State", imageFileName: "montana-physical-map", visited: false) )],
         
-    [( (name: "Nebraska", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Nevada", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "New Hampshire", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "New Jersey", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "New Mexico", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "New York", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "North Carolina", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "North Dakota", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Nebraska", nickname: "Cornhusker State", imageFileName: "nebraska-physical-map", visited: false) ),
+        ( (name: "Nevada", nickname: "Silver State", imageFileName: "nevada-physical-map", visited: false) ),
+        ( (name: "New Hampshire", nickname: "Granite State", imageFileName: "new-hampshire-physical-map", visited: false) ),
+        ( (name: "New Jersey", nickname: "Garden State", imageFileName: "new-jersey-physical-map", visited: false) ),
+        ( (name: "New Mexico", nickname: "Land of Enchantment", imageFileName: "new-mexico-physical-map", visited: false) ),
+        ( (name: "New York", nickname: "Emipire State", imageFileName: "new-york-physical-map", visited: false) ),
+        ( (name: "North Carolina", nickname: "Old North State", imageFileName: "north-carolina-physical-map", visited: false) ),
+        ( (name: "North Dakota", nickname: "Peace Garden State", imageFileName: "north-dakota-physical-map", visited: false) )],
         
-    [( (name: "Ohio", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Oklahoma", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Oregon", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Ohio", nickname: "Buckeye State", imageFileName: "ohio-physical-map", visited: false) ),
+        ( (name: "Oklahoma", nickname: "Sooner State", imageFileName: "oklahoma-physical-map", visited: false) ),
+        ( (name: "Oregon", nickname: "Beaver State", imageFileName: "oregon-physical-map", visited: false) )],
         
-    [( (name: "Pennsylvania", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Pennsylvania", nickname: "Keystone State", imageFileName: "pennsylvania-physical-map", visited: false) )],
         
-    [( (name: "Rhode Island", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Rhode Island", nickname: "Ocean State", imageFileName: "rhode-island-physical-map", visited: false) )],
         
-    [( (name: "South Carolina", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "South Dakota", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "South Carolina", nickname: "Palemetto State", imageFileName: "south-carolina-physical-map", visited: false) ),
+        ( (name: "South Dakota", nickname: "The Mount Rushmore State", imageFileName: "south-dakota-physical-map", visited: false) )],
         
-    [( (name: "Tennessee", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Texas", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Tennessee", nickname: "Volunteer State", imageFileName: "tennessee-physical-map", visited: false) ),
+        ( (name: "Texas", nickname: "Lone State State", imageFileName: "texas-physical-map", visited: false) )],
         
-    [( (name: "Utah", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Utah", nickname: "Beehive State", imageFileName: "utah-physical-map", visited: false) )],
         
-    [( (name: "Vermont", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Virginia", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )],
+    [( (name: "Vermont", nickname: "Green Mountatin State", imageFileName: "vermont-physical-map", visited: false) ),
+        ( (name: "Virginia", nickname: "The Old Dominion", imageFileName: "virginia-physical-map", visited: false) )],
         
-    [( (name: "Washington", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "West Virginia", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Wisconsin", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) ),
-        ( (name: "Wyoming", nickname: "The Last Frontier", imageFileName: "alaska-map.jpg", visited: false) )]]
+    [( (name: "Washington", nickname: "Evergreen State", imageFileName: "washington-physical-map", visited: false) ),
+        ( (name: "West Virginia", nickname: "Mountain Stae", imageFileName: "west-virginia-physical-map", visited: false) ),
+        ( (name: "Wisconsin", nickname: "America's Dairyland", imageFileName: "wisconsin-physical-map", visited: false) ),
+        ( (name: "Wyoming", nickname: "Equality State", imageFileName: "wyoming-physical-map", visited: false) )]]
     var currentNumberOfStatesVisited = 0
     var sectionHeaders = ["A", "C", "D", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W"]
     var data: stateTuple!
@@ -87,16 +87,25 @@ class CustomTableViewController: UITableViewController {
     @IBOutlet weak var VistedIndicator: UIBarButtonItem!
     
     @IBAction func ResetButton(sender: UIBarButtonItem) {
-        currentNumberOfStatesVisited = 0;
-        VistedIndicator.title = "Visited: " + String(currentNumberOfStatesVisited)
         
-        for var i = 0; i < stateArray.count; i++ {
-            for var j = 0; j < stateArray[i].count; j++ {
-                stateArray[i][j].visited = false;
+        let alert = UIAlertController(title: "Reset All?", message: "Are you sure you want to reset all?", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { action in
+            self.currentNumberOfStatesVisited = 0;
+            self.VistedIndicator.title = "Visited: " + String(self.currentNumberOfStatesVisited)
+            
+            for var i = 0; i < self.stateArray.count; i++ {
+                for var j = 0; j < self.stateArray[i].count; j++ {
+                    self.stateArray[i][j].visited = false;
+                }
             }
-        }
-        self.tableView.reloadData()
+            self.tableView.reloadData()
+
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .Default, handler: { action in
+        }))
+        self.presentViewController(alert, animated: true, completion: nil)
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -166,15 +175,24 @@ class CustomTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if stateArray[indexPath.section][indexPath.item].visited {
-            currentNumberOfStatesVisited--
-            stateArray[indexPath.section][indexPath.item].visited = false;
+            let alert = UIAlertController(title: "Reset State?", message: "Are you sure you want to reset visiting " + self.stateArray[indexPath.section][indexPath.item].name + "?", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { action in
+                self.currentNumberOfStatesVisited--
+                self.stateArray[indexPath.section][indexPath.item].visited = false;
+                self.VistedIndicator.title = "Visited: " + String(self.currentNumberOfStatesVisited)
+                let index = NSIndexPath(forRow: indexPath.row, inSection: indexPath.section)
+                self.tableView.reloadRowsAtIndexPaths([index], withRowAnimation: UITableViewRowAnimation.Fade)
+            }))
+            alert.addAction(UIAlertAction(title: "No", style: .Default, handler: { action in
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
         } else {
             currentNumberOfStatesVisited++
             stateArray[indexPath.section][indexPath.item].visited = true;
+            VistedIndicator.title = "Visited: " + String(currentNumberOfStatesVisited)
+            let index = NSIndexPath(forRow: indexPath.row, inSection: indexPath.section)
+            self.tableView.reloadRowsAtIndexPaths([index], withRowAnimation: UITableViewRowAnimation.Fade)
         }
-        VistedIndicator.title = "Visited: " + String(currentNumberOfStatesVisited)
-        let index = NSIndexPath(forRow: indexPath.row, inSection: indexPath.section)
-        self.tableView.reloadRowsAtIndexPaths([index], withRowAnimation: UITableViewRowAnimation.Fade)
     }
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
--- WIA/MWAI1_Lea_Nathan/MWAI1_Lea_Nathan/ViewController.swift
+++ WIA/MWAI1_Lea_Nathan/MWAI1_Lea_Nathan/ViewController.swift
@@ -6,6 +6,14 @@
 //  Copyright © 2015 Okstate. All rights reserved.
 //
 
+/********************************
+*
+* IMAGE NOTICE:
+*    ALL IMAGES ARE TAKEN FROM
+*    geology.com
+*
+********************************/
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,12 +22,34 @@ class ViewController: UIViewController {
     var stateImagePath = ""
 
     @IBOutlet weak var StateImageView: UIImageView!
+    internal var imageURLString: String = "http://geology.com/state-map/maps/"
+    
+    // Create the serial queue for our web access.
+    var webQueue = dispatch_queue_create("Web Queue", DISPATCH_QUEUE_SERIAL)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = stateName
 //        StateImageView.image =
+        
+        // Create the NSURL object for the image path.
+        if let url = NSURL(string: imageURLString+stateImagePath+".gif") {
+            dispatch_async(webQueue) { () in
+                // This following "if let" is a blocking call!
+                if let data = NSData(contentsOfURL: url) {
+                    // Now that we have successfully retrieved the image, we need to
+                    // create a task on the main queue to populate the image view.
+                    dispatch_async(dispatch_get_main_queue()) { () in
+                        // Set the content mode of the image view.
+                        self.StateImageView.contentMode = UIViewContentMode.ScaleAspectFit
+                        // Convert the data to an image and populate the image view.
+                        self.StateImageView.image = UIImage(data: data)
+                    }
+                }
+            }
+        }
+
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -28,7 +58,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
