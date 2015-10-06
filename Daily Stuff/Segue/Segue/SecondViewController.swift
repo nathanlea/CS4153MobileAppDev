3f5327e4d514a12b4d1015cf3189f28d307a7680
LEA NATHAN H <nathahl@mac13.cs.okstate.edu>
2015-10-06 10:35:50 -0500

    Added Daily Stuff

---
+++ Daily Stuff/Segue/Segue/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  Segue
+//
+//  Created by LEA NATHAN H on 9/15/15.
+//  Copyright © 2015 LEA NATHAN H. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    internal var imageFileName = ""    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ Daily Stuff/Segue/Segue/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by LEA NATHAN H on 9/15/15.
+//  Copyright © 2015 LEA NATHAN H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // nothing
+        if let destVC = segue.destinationViewController as? SecondViewController {
+            
+            
+            
+        }
+    }
+
+}
+
